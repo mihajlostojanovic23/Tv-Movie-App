24d065dc5fe0d01d7481fedd7cec83192d017e87
Mihajlo Stojanovic <mihajlo.s@shindiristudio.com>
2023-06-05 16:48:39 +0200

    added notFound image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getImageUrl } from '../../api/axiosInstance';
 import { withFocusable } from '@noriginmedia/react-spatial-navigation';
-
+import NotFound from '../../assets/img/notfound.jpg';
 //Interface
 interface ICard {
   data: {
@@ -13,15 +13,20 @@ interface ICard {
   focused: boolean;
 }
 
-
 function Card({ data, focused }: ICard) {
   return (
     <div className="h-[330px] transition-all duration-500 ease-in-out">
       <img
+        width={400}
+        height={225}
         className={`${
           focused ? 'border-4 border-sky-500' : 'border-4 border-transparent'
         }`}
-        src={getImageUrl(data.backdrop_path)}
+        src={
+          data.backdrop_path !== null
+            ? getImageUrl(data.backdrop_path)
+            : NotFound
+        }
       />
       <span className={` text-[white] justify-center flex text-[20px] py-2 `}>
         {data.original_title}
